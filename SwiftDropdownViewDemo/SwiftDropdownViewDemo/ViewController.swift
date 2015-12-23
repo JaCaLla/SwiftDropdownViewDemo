52b0d532acce623dac8e81cbb926ef7aaeec438f
JAVIER CALATRAVA LLAVERIA <javi.calatrava@gmail.com>
2015-12-23 08:07:55 +0100

    Updated latest changes.

--- SwiftDropdownViewDemo/SwiftDropdownViewDemo/ViewController.swift
+++ SwiftDropdownViewDemo/SwiftDropdownViewDemo/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnFoldButton: UIButton!
+    @IBOutlet weak var cnsUpperViewHeight: NSLayoutConstraint!
+    
+    
+    var heightUpperView:CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        heightUpperView = self.cnsUpperViewHeight.constant;
+        
+        self.setupUIControls()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupUIControls(){
+        
+        self.btnFoldButton.addGestureRecognizer(UIPanGestureRecognizer.init(target: self, action: "panDetected:"))
+    }
+    
+    
+    func panDetected(recognizer: UIPanGestureRecognizer){
+        
+        if(recognizer.state == UIGestureRecognizerState.Began){
+            heightUpperView = self.cnsUpperViewHeight.constant
+        }
+        
+        let point:CGPoint = recognizer.translationInView(self.btnFoldButton.superview)
+        
+        let newHeight =  heightUpperView + point.y
+        
+        if(newHeight >= UIScreen.mainScreen().bounds.height * 0.15 &&
+            newHeight <= UIScreen.mainScreen().bounds.height * 0.66){
+                cnsUpperViewHeight.constant = newHeight
+        }
+        
+        //print("\(point)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
